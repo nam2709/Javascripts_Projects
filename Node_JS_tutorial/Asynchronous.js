627526ced6b9e1c8f5fb80eb3117b97daa92a395
nam2709 <9namancam@gmail.com>
2023-09-05 13:56:31 +0700

    first update

---
+++ Node_JS_tutorial/Asynchronous.js
@@ -0,0 +1,3 @@
+setTimeout(() => console.log('🐇 finishes'), 1000); // IT THE callback function is call function after the timeout
+console.log('🐢 finishes');
+
---
+++ Node_JS_tutorial/event-loop.js
@@ -0,0 +1,6 @@
+while(!shouldExit) { //!shouldExit là trường hợp để Exit chưa xảy ra thì While vẫn chạy
+    processEvents();
+} 
+
+// ví dụ setTimeOut(callbackOne,1000);
+// Timers callback
